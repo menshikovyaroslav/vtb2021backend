39ebd108e67309dfd77b0aa5ef20d203160f370b
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-10-09 23:59:07 +0700

    installed cors, added main controller

---
+++ Vtb/vtb_api/Classes/Attributes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vtb_api.Classes
+{
+    /// <summary>
+    /// Аттрибут для фронтенда
+    /// </summary>
+    public class AllowCrossSiteJson : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Переопределен метод для добавления заголовка
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Vtb/vtb_api/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vtb_api.Classes;
+
+namespace vtb_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [Route("api/getdatasets")]
+        [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+        [AllowCrossSiteJson]
+        [HttpGet]
+        public ActionResult<string> GetDatasets()
+        {
+
+
+            return Ok("success");
+        }
+    }
+}
